d51fc4c0b98a86d79a2e032144d5aace6b7a97fc
madhukayal88-glitch <madhukayal88@gmail.com>
2025-12-21 05:17:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,46 @@
 class Animal {
-  #species;
-
   constructor(species) {
-    this.#species = species;
+    this._species = species;
   }
 
+  // Getter for species
   get species() {
-    return this.#species;
+    return this._species;
   }
 
+  // Method to log the animal's sound
   makeSound() {
-    console.log(`The ${this.#species} makes a sound`);
+    console.log(`The ${this.species} makes a sound`);
   }
 }
 
 class Cat extends Animal {
+  constructor(species) {
+    // Pass species to the parent Animal constructor
+    super(species);
+  }
+
   purr() {
     console.log("purr");
   }
 }
 
 class Dog extends Animal {
+  constructor(species) {
+    // Pass species to the parent Animal constructor
+    super(species);
+  }
+
   bark() {
     console.log("woof");
   }
 }
+
+// Example usage to verify:
+const myCat = new Cat("Siamese");
+myCat.makeSound(); // The Siamese makes a sound
+myCat.purr();      // purr
+
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); // The Golden Retriever makes a sound
+myDog.bark();      // woof
